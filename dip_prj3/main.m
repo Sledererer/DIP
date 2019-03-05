b0c06d7d827f70bd9fc4507397a5c6f750898404
Sledererer <36487110+Sledererer@users.noreply.github.com>
2019-03-05 09:13:46 -0600

    snakeMap included

--- dip_prj3/main.m
+++ dip_prj3/main.m
@@ -1,5 +1,16 @@
-function main()
-    input_image = imread('rose512.tif');
-    snakeForce4e(input_image);
-end
+%Zac Ferris
+%Stephen Lederer
 
+function main()
+
+    input_image = 'rose512.tif';
+    threshold = 0.05;%0-1 or 'auto'
+    sig = 1;%any number
+    nsig = 2;%any number
+    order = 'after';%'before', 'after', 'both', 'none'
+    
+    %Provide input image, with optional threshold, and optional
+    %{sig,nsig,order}
+    emap = snakeMap4e(input_image, threshold, sig, nsig, 'after');
+    snakeForce4e(emap);
+end
